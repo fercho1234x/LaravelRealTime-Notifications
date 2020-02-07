02ae5dca9d202142e858479287b2ebcc1ad895b9
Fernando Bautista <fernando-bautista1@hotmail.com>
2020-02-07 00:02:17 -0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,39 @@
+require('./bootstrap');
+
+// Canal publico, definido en nuestro evento
+
+console.log( 'hola mundo' );
+
+/*
+Canal Publico
+
+Echo.channel( 'notifications' )
+	.listen('UserSessionChanged', ( e ) => {
+		const notificationElement = document.querySelector( '#notifications' );
+
+		notificationElement.innerText = e.message;
+
+		notificationElement.classList.remove( 'invisible' );
+		notificationElement.classList.remove( 'alert-success' );
+		notificationElement.classList.remove( 'alert-danger' );
+
+		notificationElement.classList.add( `alert-${ e.type }` )
+	});
+*/
+
+/*
+Canal Privado
+*/
+
+Echo.private( 'notifications' )
+	.listen('UserSessionChanged', ( e ) => {
+		const notificationElement = document.querySelector( '#notifications' );
+
+		notificationElement.innerText = e.message;
+
+		notificationElement.classList.remove( 'invisible' );
+		notificationElement.classList.remove( 'alert-success' );
+		notificationElement.classList.remove( 'alert-danger' );
+
+		notificationElement.classList.add( `alert-${ e.type }` )
+	});
